12c305e4c645871772114c574d24752f29b091a6
msauerberg <50632204+msauerberg@users.noreply.github.com>
2025-07-17 14:12:06 +0000

    Deploying to gh-pages from @ msauerberg/msauerberg.github.io@31f85e6d5f7c80ae9707a46d811928ba966ed5c1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,11 +48,11 @@ ninja.data = [{
             window.location.href = "/blog/2025/kitesurfing/";
           
         },
-      },{id: "post-test-text",
+      },{id: "post-sailing-summer-2025",
         
-          title: "Test text",
+          title: "Sailing, summer 2025",
         
-        description: "text on something",
+        description: "sailing",
         section: "Posts",
         handler: () => {
           
@@ -64,16 +64,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-new-webpage-online",
+          title: 'New webpage online!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
